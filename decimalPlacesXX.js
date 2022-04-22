b252179974cffbecacf15010e8deb7835735b396
fadyehabamer <fadyamer45@gmail.com>
2022-04-22 20:06:59 +0200

    decimal places

--- decimalPlacesXX.js
+++
@@ -1,7 +0,0 @@
-function DecimalPlaces(num) {
-  // return the number of digits after the decimal point
-  // let x = num.toString().split('.')[1].length || 0;
-  console.log(5.0 == 5);
-  // return parseInt(x);
-}
-console.log(DecimalPlaces(5.0));
